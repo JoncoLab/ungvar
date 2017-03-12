e79ab4d71632c63ad16bed917a8f5502020ab54f
VasyaPhilis <urologept@gmail.com>
2017-03-12 21:47:34 +0200

    Щось хоча б
    Бля, не хочу писати одно і то же - див. Summary

--- src/scripts/js/catalog.js
+++ src/scripts/js/catalog.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 // var main;
 // main = function () {
 //     var header = $('header'),
@@ -14,4 +14,32 @@ $(document).ready(function () {
   $('#input1').change(function () {
     $('#input2').val($(this).val());
   });
+
+
+  var avatarElem = document.getElementById('page'),
+      avatarSourceTop = avatarElem.getBoundingClientRect().top + window.pageYOffset;
+    window.onscroll = function() {
+        if (avatarElem.classList.contains('fixed') && window.pageYOffset < avatarSourceTop) {
+            avatarElem.classList.remove('fixed');
+        } else if (window.pageYOffset > avatarSourceTop) {
+            avatarElem.classList.add('fixed');
+        }
+    };
+
+  var showingCatalog = function () {
+      $('.lending-nav td').click(function () {
+          $('.lending-nav').css('display', 'none');
+
+          $('.page-navigation').css({
+                  'visibility': 'visible'
+       });
+
+          $('.content').css('display', 'table');
+      });
+  };
+  showingCatalog();
+
+  var catalogMarginTop = $('.page-navigation ul').css('height');
+    $('table.content').css('margin-top', catalogMarginTop);
+
 });
