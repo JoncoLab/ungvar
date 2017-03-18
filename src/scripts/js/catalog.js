0698d669058c0b6a9caf4d577716d6f78e38ec0d
VasyaPhilis <urologept@gmail.com>
2017-03-18 18:23:05 +0200

    nav done

--- src/scripts/js/catalog.js
+++ src/scripts/js/catalog.js
@@ -25,29 +25,46 @@ $(document).ready(function () {
       avatarSourceTop = avatarElem.getBoundingClientRect().top + window.pageYOffset;
     window.onscroll = function() {
         if (avatarElem.classList.contains('fixed') && window.pageYOffset < avatarSourceTop) {
-            avatarElem.classList.remove('fixed');
+            avatarElem.classList.remove('fixed'),
+            $('#start').css('visibility', 'hidden');
         } else if (window.pageYOffset > avatarSourceTop) {
-            avatarElem.classList.add('fixed');
+            avatarElem.classList.add('fixed'),
+            $('#start').css('visibility', 'visible');
         }
+
     };
+
+    var liftSize = $('header').css('height');
+    $('#start').click(function () {
+        window.scrollTo(0, liftSize);
+    });
+
 
   var showingCatalog = function () {
       $('.lending-nav td').click(function () {
           $('.lending-nav').css('display', 'none');
+          $(".category").css('display', 'block');
 
           $('.page-navigation').css({
                   'visibility': 'visible'
        });
-
-          $('.content').css('display', 'table');
-          $('#DataTables_Table_0_wrapper ').css('display', 'block');
       });
-
   };
   showingCatalog();
+    $('#category-button').click(function () {
+        $('.category-nav').css('display', 'inline-flex');
+    })
+    $('.category-nav').mouseleave(function () {
+        $(this).fadeOut('fast');
+    });
+
 
   var catalogMarginTop = $('.page-navigation ul').css('height');
-    $('table.content').css('margin-top', catalogMarginTop);
+    $('section.category').css('margin-top', catalogMarginTop);
+
+
+
+
 
 
 
